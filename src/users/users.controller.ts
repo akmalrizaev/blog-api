a9d5a0cb1c4238d774eea7763dbe9b5ff5b8d25c
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-01-10 16:06:42 +0500

    findAll Users Method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -31,7 +31,7 @@ export class UsersController {
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    return 'You sent a get request to users endpoint';
+    return this.usersService.findAll(getUserParamDto, limit, page);
   }
 
   @Post()
