ba489c965c28b595a68f806f9e3bd1ce9bb2e580
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-01-13 17:43:08 +0500

    Circular Dependency

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, forwardRef, Inject } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    // Injecting UsersServe
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+  public login(email: string, password: string, id: string) {
+    // Check user exists database
+    // login
+    // token
+    const user = this.usersService.findOneById('1234');
+    return 'SAMPLE_TOKEN';
+  }
+
+  public isAuth() {
+    return true;
+  }
+}
