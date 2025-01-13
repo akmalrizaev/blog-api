3dd68f2a415075bc22623e3176694acf238eb09e
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-01-13 15:37:33 +0500

    Return Posts From Posts Service

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -3,6 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PostsService {
   public findAll(userId: string) {
-    console.log(userId);
+    // Users Service
+    // Find a User
+
+    return [
+      {
+        title: 'Test Title',
+        content: 'Test Content',
+      },
+      {
+        title: 'Test Title 2',
+        content: 'Test Content 2',
+      },
+    ];
   }
 }
